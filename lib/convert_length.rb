45e8f0049017b4806ec058c402747661c56bbc8b
shunkatagiri <shun.katagiri1214@gmail.com>
2023-07-17 15:10:30 +0900

    見やすいように修正

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,4 @@
 def convert_length(length, unit_form, unit_to)
-  units = { 'm' => 1, 'ft' => 3.28, 'in' => 39.37 }
+  units = { m: 1, ft: 3.28, in: 39.37 }
   ( length / units[unit_form] * units[unit_to]).round(2)
 end
