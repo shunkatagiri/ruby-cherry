464accdc63e5fd46d2b8b0af8909ab844b4422df
shunkatagiri <shun.katagiri1214@gmail.com>
2023-07-17 14:25:41 +0900

    convert_lengthメソッドを追加’ q exit

--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
-
+require_relative '../lib/convert_length'
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
-    assert_equal 39.37, test_convert_length(1, 'm', 'in')
+    assert_equal 39.37, convert_length(1, 'm', 'in')
   end
 end
